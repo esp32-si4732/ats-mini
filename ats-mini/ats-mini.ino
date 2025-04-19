ea80629c02d9a7c6d683169baff1cfdddc98ed28
Max Arnold <arnold.maxim@gmail.com>
2025-04-19 20:09:08 +0700

    Display test 1

--- ats-mini/ats-mini.ino
+++ ats-mini/ats-mini.ino
@@ -142,9 +142,9 @@ void setup()
   // https://github.com/esp32-si4732/ats-mini/issues/41
   // if(tft.readcommand8(ST7789_RDDID, 3) == 0x93)
   // {
-    // tft.invertDisplay(0);
-    // tft.writecommand(TFT_MADCTL);
-    // tft.writedata(TFT_MAD_MV | TFT_MAD_MX | TFT_MAD_MY | TFT_MAD_COLOR_ORDER);
+    tft.invertDisplay(1);
+    tft.writecommand(TFT_MADCTL);
+    tft.writedata(TFT_MAD_MV | TFT_MAD_MX | TFT_MAD_MY | TFT_MAD_COLOR_ORDER);
   // }
 
   tft.fillScreen(TH.bg);
