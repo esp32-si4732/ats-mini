7e93a3fdd7e3da97d42ccf6411c99034357d178f
Max Arnold <arnold.maxim@gmail.com>
2026-09-01 19:20:31 +0700

    Switch from TFT_eSPI & PNGdec to LovyanGFX

--- ats-mini/ats-mini.ino
+++ ats-mini/ats-mini.ino
@@ -103,8 +103,8 @@ uint8_t  snr  = 0;
 //
 Rotary encoder  = Rotary(ENCODER_PIN_B, ENCODER_PIN_A, encoderHalfStep);
 ButtonTracker pb1 = ButtonTracker();
-TFT_eSPI tft    = TFT_eSPI();
-TFT_eSprite spr = TFT_eSprite(&tft);
+LGFX tft;
+LGFX_Sprite spr(&tft);
 SI4735_fixed rx;
 
 //
@@ -146,37 +146,15 @@ void setup()
   ledcWrite(PIN_LCD_BL, 0);          // Default value 0%
 
   // TFT display setup
-  tft.begin();
+  tft.init();
   tft.setRotation(3);
 
-  #if !defined(LILYGO_SI473X)
-  // Detect and fix the mirrored & inverted display
-  // https://github.com/esp32-si4732/ats-mini/issues/41
-  uint8_t did3 = tft.readcommand8(ST7789_RDDID, 3);
-  // 0x048181B3 - the original display
-  // 0x04858552 - high gamma display
-  // 0x00009307 - inverted & mirrored display
-  if(did3 == 0x93)
-  {
-    tft.invertDisplay(0);
-    tft.writecommand(TFT_MADCTL);
-    tft.writedata(TFT_MAD_MV | TFT_MAD_MX | TFT_MAD_MY | TFT_MAD_BGR);
-  }
-  else if(did3 == 0x85)
-  {
-    tft.writecommand(0x26); // GAMSET
-    tft.writedata(8);       // Gamma Curve 3
-
-    tft.writecommand(0x55); // WRCACE (content adaptive brightness and color)
-    tft.writedata(0xB1);    // High enhancement, UI mode
-  }
-  #endif
-
   tft.fillScreen(TH.bg);
+  spr.setPsram(true);
+  spr.setColorDepth(16);
   spr.createSprite(320, 170);
   spr.setTextDatum(MC_DATUM);
-  spr.setSwapBytes(true);
-  spr.setFreeFont(&Orbitron_Light_24);
+  spr.setFont(&lgfx::fonts::Orbitron_Light_24);
   spr.setTextColor(TH.text, TH.bg);
 
   // Press and hold Encoder button to force an preferences reset
