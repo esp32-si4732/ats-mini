1e565d59932b2170e512f9aa66d71f3881c6c563
Max Arnold <arnold.maxim@gmail.com>
2026-09-16 15:17:44 +0700

    Move Scan and Memory buffers to PSRAM

--- ats-mini/Menu.cpp
+++ ats-mini/Menu.cpp
@@ -181,10 +181,10 @@ int getTotalModes() { return(ITEM_COUNT(bandModeDesc)); }
 //
 
 uint8_t memoryIdx = 0;
-Memory memories[MEMORY_COUNT];
+Memory *memories = nullptr;
 Memory newMemory;
 
-int getTotalMemories() { return(ITEM_COUNT(memories)); }
+int getTotalMemories() { return(MEMORY_COUNT); }
 
 //
 // RDS Menu
@@ -844,14 +844,14 @@ bool tuneToMemory(const Memory *memory)
 
 static void doMemory(int16_t enc)
 {
-  memoryIdx = wrap_range(memoryIdx, enc, 0, LAST_ITEM(memories));
+  memoryIdx = wrap_range(memoryIdx, enc, 0, getTotalMemories() - 1);
   if(!tuneToMemory(&memories[memoryIdx])) tuneToMemory(&newMemory);
 }
 
 static void clickMemory(uint8_t idx, bool shortPress)
 {
   // Must have a valid index
-  if(idx>LAST_ITEM(memories)) return;
+  if(idx>=getTotalMemories()) return;
 
   if(shortPress)
   {
@@ -1612,7 +1612,7 @@ static void drawMemory(int x, int y, int sx)
   sprintf(label_memory, "%s %2.2d", menu[MENU_MEMORY], memoryIdx + 1);
   drawCommon(label_memory, x, y, sx, true);
 
-  int count = ITEM_COUNT(memories);
+  int count = getTotalMemories();
   for(int i=-2 ; i<3 ; i++)
   {
     int j = abs((memoryIdx+count+i)%count);
