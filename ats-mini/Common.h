62de2f66bb88e80d331c4273ec144285a03da472
Danucosukosuko <pb.lopezguzman@gmail.com>
2026-09-20 11:49:30 +0200

    Add a forced mono setting for FM

--- ats-mini/Common.h
+++ ats-mini/Common.h
@@ -40,6 +40,11 @@ static constexpr const lgfx::IFont* FONT_DIGITS  = &lgfx::fonts::Font7;  // 48px
 #define RDS_RT        0b00001000  // Radio text
 #define RDS_PT        0b00010000  // Program type
 #define RDS_RBDS      0b00100000  // Use US PTYs
+#define RDS_AF        0b01000000  // Follow alternative frequencies
+
+// FM stereo modes
+#define FM_STEREO_AUTO 0 // Let the receiver blend down to mono on weak signals
+#define FM_STEREO_MONO 1 // Force mono audio
 
 // Sleep modes
 #define SLEEP_LOCKED   0 // Lock the encoder
@@ -202,6 +207,7 @@ extern uint8_t tcpModeIdx;
 extern uint8_t bleModeIdx;
 extern uint8_t wifiModeIdx;
 extern uint8_t FmRegionIdx;
+extern uint8_t fmStereoIdx;
 
 extern int8_t agcIdx;
 extern int8_t agcNdx;
